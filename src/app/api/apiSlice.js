4f8d916c077330f62845fccc30e617a769bf6e1b
AR-Rahat <asfakurrahat7@gmail.com>
2023-05-04 08:57:55 +0600

    ok

--- src/app/api/apiSlice.js
+++ src/app/api/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 import { setCredentials } from '../../features/auth/authSlice'
 
 const baseQuery = fetchBaseQuery({
-    baseUrl: 'https://mern-api-h7ya.onrender.com/',
+    baseUrl: 'https://mern-api-h7ya.onrender.com',
     credentials: 'include',
     prepareHeaders: (headers, { getState }) => {
         const token = getState().auth.token
